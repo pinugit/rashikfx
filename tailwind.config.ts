c2c205f9e84a9a2d8cfc37f3b6de22ab6316944c
pinugit <pinuwork@gmail.com>
2023-10-09 13:44:51 +0530

    added daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#001c93",
+
+          secondary: "#2a79e9",
+
+          accent: "#292954",
+
+          neutral: "#f6f6fb",
+
+          "base-100": "#17191a",
+
+          info: "#8be8fd",
+
+          success: "#52fa7c",
+
+          warning: "#f1fa89",
+
+          error: "#ff5757",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
+export default config;
