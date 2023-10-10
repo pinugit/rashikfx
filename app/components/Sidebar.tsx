ec887719701b6462b3ca44c3ee9769c0065594c2
pinugit <pinuwork@gmail.com>
2023-10-10 16:30:15 +0530

    reformated teh code

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarCard from "./SidebarCard";
 
 const Sidebar = () => {
   return (
-    <div className="w-[20%] h-screen fix ed z-50 right-0 bg-base-100 p-3 flex flex-col gap-6">
+    <div className="w-[20%] h-screen fixed z-50 right-0 bg-base-100 p-3 flex flex-col gap-6">
       <ProfileView />
       <SidebarCard />
     </div>
